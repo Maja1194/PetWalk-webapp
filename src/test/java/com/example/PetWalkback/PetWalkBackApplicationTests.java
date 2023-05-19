76ba50e886c57aa235708583a15826e13a247d39
Maja Račić <maja.racic@student.etf.unibl.org>
2023-05-19 12:56:58 +0200

    Zavrsna verzija

---
+++ src/test/java/com/example/PetWalkback/PetWalkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetWalkback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetWalkBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
